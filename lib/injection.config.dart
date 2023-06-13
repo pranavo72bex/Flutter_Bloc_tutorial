24c4ba007dc859ebfd9c67271d4f4dbacf26d338
pranavo72bex <pranav072bex@ioepc.edu.np>
2023-06-13 16:29:36 +0545

    chore: minor changes

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -6,8 +6,14 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart'
-    as _i2; // ignore_for_file: unnecessary_lambdas
+import 'package:injectable/injectable.dart' as _i2;
+
+import 'clean_architecture/application/application_life_cycle/application_life_cyle_cubit.dart'
+    as _i3;
+import 'clean_architecture/application/permission/permission_cubit.dart' as _i6;
+import 'clean_architecture/domain/permission/I_permission_service.dart' as _i4;
+import 'clean_architecture/infrastructure/permission/permission_service.dart'
+    as _i5; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -21,5 +27,12 @@ _i1.GetIt $initGetIt(
     environment,
     environmentFilter,
   );
+  gh.lazySingleton<_i3.ApplicationLifeCyleCubit>(
+      () => _i3.ApplicationLifeCyleCubit());
+  gh.lazySingleton<_i4.IPermissionService>(() => _i5.PermissionService());
+  gh.lazySingleton<_i6.PermissionCubit>(() => _i6.PermissionCubit(
+        get<_i4.IPermissionService>(),
+        get<_i3.ApplicationLifeCyleCubit>(),
+      ));
   return get;
 }
