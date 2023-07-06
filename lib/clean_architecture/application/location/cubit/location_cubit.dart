67abf38160a8a2d4d01c5decca2a7531a69eb256
pranavo72bex <pranav072bex@ioepc.edu.np>
2023-07-06 17:54:16 +0545

    chore: minor change

--- lib/clean_architecture/application/location/cubit/location_cubit.dart
+++ lib/clean_architecture/application/location/cubit/location_cubit.dart
@@ -1,14 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_example/clean_architecture/application/permission/permission_cubit.dart';
 import 'package:bloc_example/clean_architecture/domain/location/i_location_service.dart';
 import 'package:bloc_example/clean_architecture/domain/location/location_model.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 
 part 'location_state.dart';
 part 'location_cubit.freezed.dart';
 
+@injectable
 class LocationCubit extends Cubit<LocationState> {
   final IlocationService _locationService;
-  LocationCubit(this._locationService) : super(LocationState.initial()) {
-    _locationService.positionStream.listen((event) {});
+  final PermissionCubit _permissionCubit;
+  LocationCubit(this._locationService, this._permissionCubit)
+      : super(LocationState.initial()) {
+    _locationService.positionStream.listen((location) {
+      emit(state.copyWith(userLocation: location));
+    });
   }
 }
